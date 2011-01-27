c112f615f9a5dbd6e6efcc73c92114c96839d7cd
Sapu94 <sapu94@gmail.com>
2011-01-27 00:06:37 +0000

    fixed localization files so the proper language should now be loaded on non-english clients
    git-svn-id: svn://svn.curseforge.net/wow/tradeskillmaster_auctiondb/mainline/trunk@69 fbbedae0-e8d0-4955-9fe5-b6185a2b721e

--- Locale/esMX.lua
+++ Locale/esMX.lua
@@ -2,7 +2,7 @@
 -- Please use the localization app on CurseForge to update this
 -- http://wow.curseforge.com/addons/TradeSkillMaster_AuctionDB/localization/
 
-local L = LibStub("AceLocale-3.0"):NewLocale("TradeSkillMaster_AuctionDB", "esMX", true)
+local L = LibStub("AceLocale-3.0"):NewLocale("TradeSkillMaster_AuctionDB", "esMX")
 if not L then return end
 
 --@localization(locale="esMX", format="lua_additive_table")@
--- Locale/ruRU.lua
+++ Locale/ruRU.lua
@@ -2,7 +2,7 @@
 -- Please use the localization app on CurseForge to update this
 -- http://wow.curseforge.com/addons/TradeSkillMaster_AuctionDB/localization/
 
-local L = LibStub("AceLocale-3.0"):NewLocale("TradeSkillMaster_AuctionDB", "ruRU", true)
+local L = LibStub("AceLocale-3.0"):NewLocale("TradeSkillMaster_AuctionDB", "ruRU")
 if not L then return end
 
 --@localization(locale="ruRU", format="lua_additive_table")@
